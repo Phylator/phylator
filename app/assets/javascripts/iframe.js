6f4701b67f756e69a808fe378211cc1f375d96d6
jonhue <jonas.huebotter@gmx.de>
2017-09-17 15:13:32 +0200

    minor bugfixes

--- app/assets/javascripts/iframe.js
+++ app/assets/javascripts/iframe.js
@@ -1,17 +1,17 @@
-document.addEventListener( 'turbolinks:load', function() {
-    iframeInit();
-});
-
-
-
-function iframeInit() {
-
-    var inIframe = ( window.location != window.parent.location ) ? true : false;
-
-    if ( inIframe === true ) {
-        $('body').addClass('disabledTransitions');
-        $(':root').css({ 'font-size': '18px' });
-        $('body').removeClass('disabledTransitions');
-    };
-
-};
+// document.addEventListener( 'turbolinks:load', function() {
+//     iframeInit();
+// });
+//
+//
+//
+// function iframeInit() {
+//
+//     var inIframe = ( window.location != window.parent.location ) ? true : false;
+//
+//     if ( inIframe === true ) {
+//         $('body').addClass('disabledTransitions');
+//         $(':root').css({ 'font-size': '18px' });
+//         $('body').removeClass('disabledTransitions');
+//     };
+//
+// };
