57c97aa4138ca94a67a66e3d3ccc002289f32444
jonhue <jonas.huebotter@gmx.de>
2017-09-20 10:02:43 +0200

    minor bugfixes

--- app/assets/javascripts/iframe.js
+++ app/assets/javascripts/iframe.js
@@ -9,11 +9,9 @@ function iframeInit() {
     var inIframe = ( window.location != window.parent.location ) ? true : false;
 
     if ( inIframe === true ) {
-        $('body').hide();
+        $('body').addClass('disabledTransitions');
         $(':root').css({ 'font-size': '18px' });
-        setTimeout(function() {
-            $('body').fadeIn(500);
-        }, 500);
+        $('body').removeClass('disabledTransitions');
     };
 
 };
