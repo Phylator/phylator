f410c11112475abeff024c5b1547049e3b7407d3
Jonas Hübotter <jonas.huebotter@gmx.de>
2017-12-04 18:28:51 +0100

    update turbolinks-animate.js

--- app/assets/javascripts/components/turbolinks-animate.js
+++ app/assets/javascripts/components/turbolinks-animate.js
@@ -1,5 +1,5 @@
 $(document).on( 'turbolinks:load', function() {
-    $('body.turbolinks-animate').turbolinksAnimate({ 'duration': '0.35s' });
+    $('body.turbolinks-animate').turbolinksAnimate({ 'duration': '0.25s' });
     turbolinksAnimateAppear();
 });
 $(document).on( 'turbolinks:request-start', function() {
