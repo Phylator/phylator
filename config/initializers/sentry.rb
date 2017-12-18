18cbf14976a82d60bf0ac30e3fc40abb2858c20a
Jonas Hübotter <jonas.huebotter@gmx.de>
2017-12-18 06:39:21 +0100

    update sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,6 @@
 Rails.application.config.filter_parameters << :password
 
 Raven.configure do |config|
-    config.dsn = 'https://0efff54097da44aeb91bf4940bc92725:90b6c1f07aa24b1388e66305a9d11efb@sentry.io/214094'
+    config.dsn = Settings.sentry_raven.dsn
     config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
 end
