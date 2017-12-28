33434f8db176584a69a51cc7dd7c621d8d5184bd
Jonas Hübotter <jonas.huebotter@gmx.de>
2017-12-28 22:00:11 +0100

    testing

--- app/assets/javascripts/components/alert.js
+++ app/assets/javascripts/components/alert.js
@@ -1,16 +1,16 @@
-// document.addEventListener( 'turbolinks:load', function() {
-//     componentsAlertInit();
-// });
-//
-//
-//
-// function componentsAlertInit() {
-//     var el = $('p#alert');
-//
-//     if ( el.text().length > 0 ) {
-//         iziToast.error({
-//             title: el.text(),
-//             backgroundColor: '#ec626e'
-//         });
-//     };
-// };
+document.addEventListener( 'turbolinks:load', function() {
+    componentsAlertInit();
+});
+
+
+
+function componentsAlertInit() {
+    var el = $('p#alert');
+
+    if ( el.text().length > 0 ) {
+        iziToast.error({
+            title: el.text(),
+            backgroundColor: '#ec626e'
+        });
+    };
+};
