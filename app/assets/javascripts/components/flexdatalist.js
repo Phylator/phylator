a44c840585aab9ba3665b37017ce54875267b15f
Jonas Hübotter <jonas.huebotter@gmx.de>
2017-12-28 22:07:56 +0100

    testing

--- app/assets/javascripts/components/flexdatalist.js
+++ app/assets/javascripts/components/flexdatalist.js
@@ -9,10 +9,10 @@ function flexdatalistInit( el = $('body') ) {
     el.find('input.flexdatalist').flexdatalist();
 
     el.find('input.flexdatalist[data-units]').on( 'change:flexdatalist', function( event, set, options ) {
-        var unitsEl = $(this).data('units');
+        var unitsEl = $(this).data('units'),
             unitsUrl = 'app/quantities/' + set.value + '/units_of_measurement.json?locale=' + $('p#lang').text();
-        $(unitsEl).data( 'data', unitsUrl )
-        $(unitsEl).flexdatalist()
+        $(unitsEl).data( 'data', unitsUrl );
+        $(unitsEl).flexdatalist();
         $(unitsEl).attr( 'disabled', 'false' );
     });
 
