c4f51b8a2b365630d69ecca3d62100d9143315ac
Jonas Hübotter <jonas.huebotter@gmx.de>
2017-12-28 21:58:42 +0100

    testing

--- app/assets/javascripts/components/timeago.js
+++ app/assets/javascripts/components/timeago.js
@@ -1,30 +1,30 @@
-document.addEventListener( 'turbolinks:load', function() {
-    componentsTimeagoInit();
-});
-
-
-
-function componentsTimeagoInit() {
-    if ( $('p#lang').text() == 'de' ) {
-        jQuery.timeago.settings.strings = {
-            prefixAgo: "vor",
-            prefixFromNow: "in",
-            suffixAgo: "",
-            suffixFromNow: "",
-            seconds: "wenigen Sekunden",
-            minute: "etwa einer Minute",
-            minutes: "%d Minuten",
-            hour: "etwa einer Stunde",
-            hours: "%d Stunden",
-            day: "etwa einem Tag",
-            days: "%d Tagen",
-            month: "etwa einem Monat",
-            months: "%d Monaten",
-            year: "etwa einem Jahr",
-            years: "%d Jahren"
-        };
-    };
-    // more translations: https://github.com/rmm5t/jquery-timeago/tree/master/locales
-
-    $('.timeago').timeago();
-};
+// document.addEventListener( 'turbolinks:load', function() {
+//     componentsTimeagoInit();
+// });
+//
+//
+//
+// function componentsTimeagoInit() {
+//     if ( $('p#lang').text() == 'de' ) {
+//         jQuery.timeago.settings.strings = {
+//             prefixAgo: "vor",
+//             prefixFromNow: "in",
+//             suffixAgo: "",
+//             suffixFromNow: "",
+//             seconds: "wenigen Sekunden",
+//             minute: "etwa einer Minute",
+//             minutes: "%d Minuten",
+//             hour: "etwa einer Stunde",
+//             hours: "%d Stunden",
+//             day: "etwa einem Tag",
+//             days: "%d Tagen",
+//             month: "etwa einem Monat",
+//             months: "%d Monaten",
+//             year: "etwa einem Jahr",
+//             years: "%d Jahren"
+//         };
+//     };
+//     // more translations: https://github.com/rmm5t/jquery-timeago/tree/master/locales
+//
+//     $('.timeago').timeago();
+// };
