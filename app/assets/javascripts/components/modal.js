cf287c276eac0a4e1809c2b8704d4e46674863a9
Jonas Hübotter <jonas.huebotter@gmx.de>
2017-12-28 22:01:07 +0100

    testing

--- app/assets/javascripts/components/modal.js
+++ app/assets/javascripts/components/modal.js
@@ -1,23 +1,23 @@
-document.addEventListener( 'turbolinks:load', function() {
-    componentsModalInit();
-});
-
-
-
-function componentsModalInit() {
-    $('.modal').iziModal({
-        width: '85%'
-    });
-    $('.md-trigger').click(function(event) {
-        event.preventDefault();
-        $('.modal' + $(this).data('modal')).iziModal('open');
-    });
-};
-
-function componentsModalOpen(el) {
-    $(el).iziModal('open');
-};
-
-function componentsModalClose(el) {
-    $(el).iziModal('close');
-};
+// document.addEventListener( 'turbolinks:load', function() {
+//     componentsModalInit();
+// });
+//
+//
+//
+// function componentsModalInit() {
+//     $('.modal').iziModal({
+//         width: '85%'
+//     });
+//     $('.md-trigger').click(function(event) {
+//         event.preventDefault();
+//         $('.modal' + $(this).data('modal')).iziModal('open');
+//     });
+// };
+//
+// function componentsModalOpen(el) {
+//     $(el).iziModal('open');
+// };
+//
+// function componentsModalClose(el) {
+//     $(el).iziModal('close');
+// };
