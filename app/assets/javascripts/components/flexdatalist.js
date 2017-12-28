933992f2ba634836a9d46448587873f0fb558362
Jonas Hübotter <jonas.huebotter@gmx.de>
2017-12-28 22:03:07 +0100

    testing

--- app/assets/javascripts/components/flexdatalist.js
+++ app/assets/javascripts/components/flexdatalist.js
@@ -1,15 +1,15 @@
-document.addEventListener( 'turbolinks:load', function() {
-    flexdatalistInit();
-});
-
-
-
-function flexdatalistInit(el = $('body')) {
-
-    el.find('input.flexdatalist').flexdatalist();
-
-    el.find('input.flexdatalist[data-units]').on( 'change:flexdatalist', function(event, set, options) {
-        $($(this).data('units')).data( 'data', ( 'app/quantities/' + set.value + '/units_of_measurement.json?locale=' + $('p#lang').text() ) ).flexdatalist().attr('disabled', 'false');
-    });
-
-};
+// document.addEventListener( 'turbolinks:load', function() {
+//     flexdatalistInit();
+// });
+//
+//
+//
+// function flexdatalistInit(el = $('body')) {
+//
+//     el.find('input.flexdatalist').flexdatalist();
+//
+//     el.find('input.flexdatalist[data-units]').on( 'change:flexdatalist', function(event, set, options) {
+//         $($(this).data('units')).data( 'data', ( 'app/quantities/' + set.value + '/units_of_measurement.json?locale=' + $('p#lang').text() ) ).flexdatalist().attr('disabled', 'false');
+//     });
+//
+// };
