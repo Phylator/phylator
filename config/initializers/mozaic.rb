7bdb1a72928420cc0e15e7e7fa986e2351486f6b
Jonas Hübotter <jonas.huebotter@gmail.com>
2018-02-06 10:26:32 +0100

    update layouts/google_analytics component

--- config/initializers/mozaic.rb
+++ config/initializers/mozaic.rb
@@ -1,6 +1,6 @@
 Mozaic.configure do |config|
 
     # Define Mozaic components
-    config.define_component 'layouts/google_analytics'
+    config.define_component 'layouts/google_analytics', tracking_id: Settings.google_analytics.tracking_id
 
 end
