1af15b6b715f01a416de42fc7dd749976324df04
Jonas Hübotter <me@jonhue.me>
2018-03-24 12:21:46 +0100

    minor bugfixes

--- app/webpack/javascripts/packages/calculations/new.js
+++ app/webpack/javascripts/packages/calculations/new.js
@@ -1,5 +1,9 @@
 import MygSlide from 'myg-slide';
 
+import $ from 'jquery';
+window.jQuery = window.$ = $;
+require('jquery-flexdatalist/jquery.flexdatalist');
+
 document.addEventListener( 'turbolinks:load', () => {
     if (document.querySelector('body.calculations.new')) {
         let slideIndex = 0;
@@ -14,5 +18,13 @@ document.addEventListener( 'turbolinks:load', () => {
                 mygSlide.flickity.select(slideIndex);
             }
         }));
+        // $('input.flexdatalist').flexdatalist();
+        $('input.flexdatalist[data-units]').on( 'change:flexdatalist', function( event, set, options ) {
+            document.querySelector('.carousel-cell:nth-child(' + (slideIndex + 1) + ') a.next').classList.remove('myg-button--disabled');
+            let unitsEl = $(this).data('units'),
+                unitsUrl = '/app/quantities/' + set.value + '/units_of_measurement.json?locale=' + $('html').attr('lang');
+            $(unitsEl).data( 'data', unitsUrl );
+            $(unitsEl).flexdatalist();
+        });
     }
 })
