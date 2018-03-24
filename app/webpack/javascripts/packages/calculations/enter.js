9a9dd10831b5b4122eced30124313ed6cd2c8358
Jonas Hübotter <me@jonhue.me>
2018-03-24 21:19:05 +0100

    minor bugfixes

--- app/webpack/javascripts/packages/calculations/enter.js
+++ app/webpack/javascripts/packages/calculations/enter.js
@@ -4,15 +4,28 @@ require('jquery-flexdatalist/jquery.flexdatalist');
 
 document.addEventListener( 'turbolinks:load', () => {
     if (document.querySelector('body.calculations.enter')) {
-        $('input.flexdatalist').on( 'change:flexdatalist', function( event, set, options ) {
-            let quantityEl = this,
-                unitEl = document.querySelector(quantityEl.dataset.units);
-            if (unitEl && quantityEl.value) {
-                let unitsUrl = '/app/quantities/' + set.value + '/units_of_measurement.json?locale=' + document.querySelector('html').getAttribute('lang');
-                unitEl.dataset.data = unitsUrl;
-                unitEl.disabled = false;
-                $(unitEl).flexdatalist();
-            }
-        });
+        init();
     }
 })
+
+function addMeasurement(data) {
+    let el = document.querySelector('p#measurement-index'),
+        index = parseInt(el.innerHTML);
+    data = data.replace(index, index + 1);
+    el.innerHTML = index + 1;
+    $('.measurement').last().after(data);
+}
+window.addMeasurement = addMeasurement;
+
+function init() {
+    $('input.flexdatalist').one( 'change:flexdatalist', function( event, set, options ) {
+        let quantityEl = this,
+            unitEl = document.querySelector(quantityEl.dataset.units);
+        if (unitEl && quantityEl.value) {
+            let unitsUrl = '/app/quantities/' + set.value + '/units_of_measurement.json?locale=' + document.querySelector('html').getAttribute('lang');
+            unitEl.dataset.data = unitsUrl;
+            unitEl.disabled = false;
+            $(unitEl).flexdatalist();
+        }
+    });
+}
