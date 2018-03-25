e0e4ef29955cd41742ae6bbbf1d0fa59a48ec4f0
Jonas Hübotter <me@jonhue.me>
2018-03-25 21:03:12 +0200

    minor improvements

--- app/controllers/equations_controller.rb
+++ app/controllers/equations_controller.rb
@@ -5,7 +5,11 @@ class EquationsController < ApplicationController
 
     def show
         turbolinks_animate 'fadeinright'
+        @quantities = @equation.quantities.with_translations(I18n.locale).order(:name)
+        @constants = @equation.constants.with_translations(I18n.locale).order(:name)
+        @calculations = @equation.calculations.where(user: current_user).order('created_at desc')
         authorize! :read, @equation
+        authorizes! :read, @calculations
         render layout: 'app/show'
     end
 
