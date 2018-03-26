5d0a292bf2a6d432567e809d5c11177eec2f4e71
Jonas Hübotter <me@jonhue.me>
2018-03-26 08:49:13 +0200

    cleanup

--- app/webpack/javascripts/packages/welcome/index.js
+++
@@ -1,5 +0,0 @@
-import $ from 'jquery';
-
-export function init() {
-    // $('.slick').slick({ infinite: false, mobileFirst: true, dots: true, arrows: false });
-};
--- app/webpack/javascripts/packages/welcome/product.js
+++
@@ -1,9 +0,0 @@
-import $ from 'jquery';
-
-export function init() {
-    $('.continue').click(function() {
-        $('html, body').animate({
-            scrollTop: $('.continue').offset().top + 75
-        }, 1000);
-    });
-};
