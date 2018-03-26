86ace29006a544f68b6b3a4f0dff3e838b4d0c6c
Jonas Hübotter <me@jonhue.me>
2018-03-26 17:15:25 +0200

    minor bugfixes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -12,7 +12,7 @@ class CategoriesController < ApplicationController
 
     def show
         turbolinks_animate 'fadeinright'
-        @packs = @category.packs.with_translations(I18n.locale).order(:name)
+        @packs = @category.packs.order(:price)
         authorize! :read, @category
         authorizes! :read, @packs
         render layout: 'app/show'
