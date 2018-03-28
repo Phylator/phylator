7ecd36fc8314917f47cd9a35fadaf10797293a0f
Jonas Hübotter <me@jonhue.me>
2018-03-28 19:20:07 +0200

    minor bugfixes

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -8,29 +8,33 @@ class PurchasesController < ApplicationController
         @purchase.amount = @purchase.pack.price
         description = I18n.t('purchases.create.description', category: @purchase.pack.category, pack: @purchase.pack.name)
 
-        charge = Stripe::Charge.create(
-            amount: (@purchase.pack.price * 100).to_i,
-            currency: 'usd',
-            description: description,
-            receipt_email: params[:receipt_email],
-            source: params[:stripeToken]
-        )
+        begin
+            charge = Stripe::Charge.create(
+                amount: (@purchase.pack.price * 100).to_i,
+                currency: 'usd',
+                description: description,
+                receipt_email: params[:receipt_email],
+                source: params[:stripeToken]
+            )
 
-        @purchase.stripe_charge_id = charge[:id]
-        @purchase.completed = true
-        @purchase.save!
+            @purchase.stripe_charge_id = charge[:id]
+            @purchase.completed = true
+            @purchase.save!
 
-        # notification = Notification.create target: current_user, object: @purchase
-        # notification[:metadata] = {
-        #     onesignal_contents: {
-        #         en: I18n.t('purchases.create.success', pack: @purchase.pack.name),
-        #         de: I18n.t('purchases.create.success', pack: @purchase.pack.name, locale: :de)
-        #     }
-        # }
-        # notification.save!
-        # notification.push :OneSignal, player_ids: notification.target.onesignal_player_ids
+            # notification = Notification.create target: current_user, object: @purchase
+            # notification[:metadata] = {
+            #     onesignal_contents: {
+            #         en: I18n.t('purchases.create.success', pack: @purchase.pack.name),
+            #         de: I18n.t('purchases.create.success', pack: @purchase.pack.name, locale: :de)
+            #     }
+            # }
+            # notification.save!
+            # notification.push :OneSignal, player_ids: notification.target.onesignal_player_ids
 
-        redirect_to @purchase.pack, notice: I18n.t('purchases.create.success')
+            redirect_to @purchase.pack, notice: I18n.t('purchases.create.success')
+        rescue Stripe::CardError => e
+            redirect_to @purchase.pack, alert: e
+        end
     end
 
 end
