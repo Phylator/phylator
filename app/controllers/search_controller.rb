2afde98190d45b3c4eff61c19d317f598ab73d5c
Jonas Hübotter <me@jonhue.me>
2018-03-28 17:48:45 +0200

    fix search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,6 +7,23 @@ class SearchController < ApplicationController
     def index
         turbolinks_animate 'fadeinright'
         @tab = params[:tab] || 'calculation'
+        if params.has_key?(:content)
+            @results = []
+            case @tab
+            when 'calculation'
+                content.each { |r| @results << Calculation.find(r['objectID']) }
+            when 'quantity'
+                content.each { |r| @results << Quantity.find(r['objectID']) }
+            when 'constant'
+                content.each { |r| @results << Constant.find(r['objectID']) }
+            when 'pack'
+                content.each { |r| @results << Pack.find(r['objectID']) }
+            end
+        end
+        respond_to do |format|
+            format.html
+            format.js
+        end
     end
 
 end
--- app/webpack/javascripts/packages/search/index.js
+++ app/webpack/javascripts/packages/search/index.js
@@ -1,13 +1,37 @@
+import $ from 'jquery';
+
 document.addEventListener( 'turbolinks:load', () => {
     if (document.querySelector('body.search.index')) {
-        let algoliasearch = require('algoliasearch');
+        const algoliasearch = require('algoliasearch');
         const tab = document.querySelector('#tab').innerHTML;
         const client = algoliasearch( algoliasearchApplicationId, algoliasearchSearchKey );
-        const index = client.initIndex( tab[0].toUpperCase() + tab.substring(1) );
         document.querySelector('input#__query').addEventListener( 'input', function() {
-            index.search( this.value, ( err, content ) => {
-                console.log(content.hits);
-            });
+            document.querySelector('.myg-grid').innerHTML = '';
+            if (this.value) {
+                search( this.value, tab );
+                if ( tab == 'quantity' ) {
+                    search( this.value, 'constant' );
+                }
+            } else {
+                render(tab);
+            }
         })
     }
 })
+
+function search( query, tab ) {
+    let index = client.initIndex( tab[0].toUpperCase() + tab.substring(1) );
+    index.search( query, ( err, content ) => {
+        if (content.hits.length > 0) {
+            console.log(content.hits);
+            render(tab);
+        } else {
+            render( tab, content.hits );
+        }
+    });
+}
+function render( tab, content = null ) {
+    $.get( '/app/search', { tab: tab, content: content }, (data) => {
+        $('.myg-grid').append(data);
+    });
+}
