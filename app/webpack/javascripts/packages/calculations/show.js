077dd6b5e5b6e47792b76dc89cef912bfc37842e
Jonas Hübotter <me@jonhue.me>
2018-03-28 20:50:12 +0200

    minor bugfixes

---
+++ app/webpack/javascripts/packages/calculations/show.js
@@ -0,0 +1,12 @@
+import $ from 'jquery';
+window.jQuery = window.$ = $;
+
+document.addEventListener( 'turbolinks:load', () => {
+    if (document.querySelector('body.calculations.show')) {
+        require('jquery-flexdatalist/jquery.flexdatalist');
+        let form = document.querySelector('#unit-conversion');]
+        $('input.flexdatalist').on( 'change:flexdatalist', ( event, set, options ) => {
+            form.submit();
+        });
+    }
+})
