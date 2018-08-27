a8f6fafe9b2dea4ece4616a56e65c029b0e1ab0e
Jonas Hübotter <me@jonhue.me>
2018-08-27 19:49:25 +0200

    fix eslint errors

--- app/webpack/javascripts/myg/slide.js
+++ app/webpack/javascripts/myg/slide.js
@@ -1,12 +1,12 @@
 import MygSlide from 'myg-slide';
 
-['turbolinks:load', 'modalist:render', 'myg-tabs:render'].forEach( (event) => {
-    document.removeEventListener( event, init );
-    document.addEventListener( event, init );
-})
+['turbolinks:load', 'modalist:render', 'myg-tabs:render'].forEach((event) => {
+  document.removeEventListener(event, init);
+  document.addEventListener(event, init);
+});
 function init() {
-    const mygSlides = MygSlide.initAll( document.querySelectorAll('.myg-slide'), {
-        prevNextButtons: false,
-        wrapAround: false
-    });
+  const mygSlides = MygSlide.initAll(document.querySelectorAll('.myg-slide'), {
+    prevNextButtons: false,
+    wrapAround: false
+  });
 }
---
+++ app/webpack/javascripts/templates/calculations/new.js
@@ -0,0 +1,20 @@
+import $ from 'jquery';
+window.jQuery = window.$ = $;
+
+document.addEventListener('turbolinks:load', () => {
+  if (document.querySelector('body.calculations.new')) {
+    require('../../vendor/jquery.flexdatalist.min');
+    let unitEl = document.querySelector('#__unit'),
+      quantityEl = document.querySelector('#__quantity');
+    $(quantityEl).flexdatalist();
+    $('input.flexdatalist').on('change:flexdatalist', (event, set, options) => {
+      if (unitEl && quantityEl.value) {
+        let unitsUrl = '/app/quantities/' + set.value + '/units_of_measurement.json?locale=' + document.querySelector('html').getAttribute('lang');
+        unitEl.dataset.data = unitsUrl;
+        unitEl.disabled = false;
+        $(unitEl).flexdatalist();
+        document.querySelector('form input.myg-button').classList.remove('myg-button--disabled');
+      }
+    });
+  }
+});
