b77044bc8274b41b19c1194c33d160b17a548fd7
Jonas Hübotter <me@jonhue.me>
2018-08-29 16:58:18 +0200

    upgrade to myg 0.13.6

--- app/webpack/javascripts/mozaic/suggestion.js
+++ app/webpack/javascripts/mozaic/suggestion.js
@@ -1,7 +1,7 @@
 document.addEventListener('turbolinks:load', () => {
   if (document.querySelector('#suggestion')) {
     let wrapper = document.querySelector('#suggestion');
-    wrapper.querySelector('.myg-button--container p > a').addEventListener('click', () => {
+    wrapper.querySelector('.myg-button__container p > a').addEventListener('click', () => {
       wrapper.style.marginTop = -wrapper.offsetHeight + 'px';
     });
   }
